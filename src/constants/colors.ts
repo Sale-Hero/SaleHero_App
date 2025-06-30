dca3a875a4c555ec7ed27dafd8c1f02e8b608063
chhong <pnci1029@gmail.com>
2025-06-30 21:41:20 +0900

    공통파일 정의

---
+++ src/constants/colors.ts
@@ -0,0 +1,15 @@
+export const colors = {
+  primary: '#3554d1', // SaleHero 메인 블루
+  secondary: '#868e96', // 아이콘, 서브 텍스트용 회색
+  black: '#212529',
+  white: '#ffffff',
+  gray1: '#f8f9fa', // 아주 밝은 배경색
+  gray2: '#f1f3f5', // 경계선, 비활성 배경
+  gray3: '#dee2e6',
+  gray4: '#ced4da',
+  gray5: '#adb5bd',
+  gray6: '#868e96',
+  gray7: '#495057',
+  gray8: '#343a40',
+  red: '#fa5252', // 오류, 삭제 등
+}; 
